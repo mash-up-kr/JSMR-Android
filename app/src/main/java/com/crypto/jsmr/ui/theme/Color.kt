eac582d0942a83d90017343714a63ac5ce97c19c
jinhokang <jinho6187@gmail.com>
2022-06-20 22:49:18 +0900

    compose 및 theme 정보 추가

---
+++ app/src/main/java/com/crypto/jsmr/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.crypto.jsmr.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFDFDFDF)
+val Grey200 = Color(0xFFC1C1C1)
+val Grey300 = Color(0xFFA5A5A5)
+val Grey400 = Color(0xFF8B8B8B)
+val Grey500 = Color(0xFF6F6F6F)
+val Grey600 = Color(0xFF555555)
+val Grey700 = Color(0xFF3D3D3D)
+val Grey800 = Color(0xFF242424)
+val Black = Color(0xFF000000)
+
+val Main100 = Color(0xFFF76159)
+
+
+val SubBlue = Color(0xFF3E6FEE)
+val SubPurple = Color(0xFF934BEF)
+val SubGreen = Color(0xFFB0E519)
+val SubYellow = Color(0xFFFFCD4D)
+val ErrorRed = Color(0xFFDF1D1D)
+
+val DarkBackground = Color(0xFF242424)
+val LightBackground = Color(0xFFF7F7F7)
