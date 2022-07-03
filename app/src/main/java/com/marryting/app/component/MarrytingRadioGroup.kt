7bbf93465c83c170a73f3bec78fefed7683919a5
JinhoKang <jinho6187@gmail.com>
2022-07-03 23:27:03 +0900

    Column 추가

--- app/src/main/java/com/marryting/app/component/MarrytingRadioGroup.kt
+++ app/src/main/java/com/marryting/app/component/MarrytingRadioGroup.kt
@@ -2,10 +2,7 @@ package com.marryting.app.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,73 +23,76 @@ import com.ui.theme.Color
 @ExperimentalMaterial3Api
 @Composable
 fun MarrytingRadioGroup(
+    modifier: Modifier = Modifier,
     items: List<String> = emptyList(),
     selectedItem: String,
     itemSelected: (String) -> Unit
 ) {
-    items.forEach { item ->
+    Column {
+        items.forEach { item ->
 
-        val selectedBorderColor = if (selectedItem == item) {
-            Color.SubGreen
-        } else {
-            Color.Grey300
-        }
+            val selectedBorderColor = if (selectedItem == item) {
+                Color.SubGreen
+            } else {
+                Color.Grey300
+            }
 
-        val backgroundColor = if (selectedItem == item) {
-            Color.SubGreen.copy(alpha = 0.16f)
-        } else {
-            Color.Grey600
-        }
+            val backgroundColor = if (selectedItem == item) {
+                Color.SubGreen.copy(alpha = 0.16f)
+            } else {
+                Color.Grey600
+            }
 
-        Box(
-            modifier = Modifier
-                .width(296.dp)
-                .padding(top = 6.dp)
-                .border(1.dp, selectedBorderColor, RoundedCornerShape(8.dp))
-                .background(backgroundColor, RoundedCornerShape(8.dp))
-                .selectable(
-                    enabled = true,
-                    selected = item == selectedItem,
-                    role = Role.RadioButton,
-                    onClick = {
-                        itemSelected(item)
-                    }
-                )
-        ) {
-            Row(modifier = Modifier.padding(20.dp)) {
-
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_radio_check_disabled),
-                    tint = if (selectedItem == item) {
-                        Color.SubGreen
-                    } else {
-                        Color.Grey500
-                    },
-                    contentDescription = ""
-                )
-                Text(
-                    modifier = Modifier.padding(start = 8.dp),
-                    text = item,
-                    color = when (selectedItem) {
-                        item -> {
-                            Color.White
+            Box(
+                modifier = Modifier
+                    .width(296.dp)
+                    .padding(top = 6.dp)
+                    .border(1.dp, selectedBorderColor, RoundedCornerShape(8.dp))
+                    .background(backgroundColor, RoundedCornerShape(8.dp))
+                    .selectable(
+                        enabled = true,
+                        selected = item == selectedItem,
+                        role = Role.RadioButton,
+                        onClick = {
+                            itemSelected(item)
                         }
-                        "" -> {
-                            Color.Grey100
+                    )
+            ) {
+                Row(modifier = Modifier.padding(20.dp)) {
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_radio_check_disabled),
+                        tint = if (selectedItem == item) {
+                            Color.SubGreen
+                        } else {
+                            Color.Grey500
+                        },
+                        contentDescription = ""
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 8.dp),
+                        text = item,
+                        color = when (selectedItem) {
+                            item -> {
+                                Color.White
+                            }
+                            "" -> {
+                                Color.Grey100
+                            }
+                            else -> {
+                                Color.Grey200
+                            }
+                        },
+                        fontStyle = FontStyle.Normal,
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.SansSerif,
+                        fontWeight = if (selectedItem == item) {
+                            FontWeight.Bold
+                        } else {
+                            FontWeight.Normal
                         }
-                        else -> {
-                            Color.Grey200
-                        }
-                    },
-                    fontStyle = FontStyle.Normal,
-                    fontSize = 16.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    fontWeight = if (selectedItem == item) {
-                        FontWeight.Bold
-                    } else {
-                        FontWeight.Normal
-                    }
-                )
+                    )
+                }
             }
         }
     }
