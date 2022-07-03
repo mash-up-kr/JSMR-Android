3ec9d80abc0a1b913dcf26f87cd528aa6d1ba6f7
JinhoKang <jinho6187@gmail.com>
2022-07-03 23:25:31 +0900

    build failed 수정

--- app/src/main/java/com/marryting/app/component/MarrytingRadioGroup.kt
+++ app/src/main/java/com/marryting/app/component/MarrytingRadioGroup.kt
@@ -11,14 +11,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.marryting.app.R
