41ed23797669f399df7444a28124999284317fb3
sooziini <soojinpar1026@gmail.com>
2022-07-07 01:30:30 +0900

    progress indicator 컴포넌트 추가

---
+++ app/src/main/java/com/marryting/app/presentation/profile/ProgressIndicator.kt
@@ -0,0 +1,46 @@
+package com.marryting.app.presentation.profile
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProgressIndicator(
+    modifier: Modifier = Modifier,
+    currentQuestionIndex: Int
+) {
+    Row(
+        modifier = modifier
+    ) {
+        for (i in 0 until 4) {
+            val backgroundColor = if (i <= currentQuestionIndex) {
+                Color.White
+            } else {
+                Color.Transparent
+            }
+
+            Box(
+                modifier = modifier
+                    .size(8.dp)
+                    .background(backgroundColor, CircleShape)
+                    .then(
+                        if (currentQuestionIndex < i) {
+                            Modifier.border(1.dp, Color.White, CircleShape)
+                        } else {
+                            Modifier
+                        }
+                    )
+            )
+            Spacer(modifier = Modifier.padding(end = 6.dp))
+        }
+    }
+}
