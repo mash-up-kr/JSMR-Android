8fa2d5329a4e6e5227d9025bd53eb85f160273a8
sooziini <soojinpar1026@gmail.com>
2022-07-10 06:36:57 +0900

    spotlessApply 적용

--- app/src/main/java/com/marryting/app/presentation/profile/RegisterScreen.kt
+++ app/src/main/java/com/marryting/app/presentation/profile/RegisterScreen.kt
@@ -1,12 +1,8 @@
 package com.marryting.app.presentation.profile
 
-import androidx.compose.foundation.background
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ui.theme.Color
 
 @Composable
 fun RegisterScreen(viewModel: RegisterViewModel = viewModel()) { // hiltViewModel()로 변경해야 함
