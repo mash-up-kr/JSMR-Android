5b7d6375e2836d955515ea6de9b30d850cd3ec0b
sooziini <soojinpar1026@gmail.com>
2022-07-10 06:12:22 +0900

    RepositoryModule 일단.. 추가

---
+++ app/src/main/java/com/marryting/app/di/RepositoryModule.kt
@@ -0,0 +1,16 @@
+package com.marryting.app.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+
+//@Module
+//@InstallIn(ActivityRetainedComponent::class)
+//interface RepositoryModule {
+//
+//    @Binds
+//    @ActivityRetainedScoped
+//    fun bindProfileRepository(profileRepository: ProfileRepository): ProfileRepository
+//}
