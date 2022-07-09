31ccf8c8724e88aa6e5955fa3523d116c4fc336d
sooziini <soojinpar1026@gmail.com>
2022-07-10 06:08:02 +0900

    Content, RegisterState 추가

---
+++ app/src/main/java/com/marryting/app/presentation/profile/RegisterState.kt
@@ -0,0 +1,29 @@
+package com.marryting.app.presentation.profile
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.marryting.app.data.profile.Content
+
+data class ContentState(
+    val content: Content,
+    val totalContentsCount: Int,
+    val showPrevious: Boolean,
+    val showDone: Boolean
+) {
+    var enabledNext: Boolean by mutableStateOf(true) // false로 해야 함
+}
+
+sealed class RegisterState {
+    object Loading : RegisterState()
+
+    data class Contents(
+        val contentsState: List<ContentState>
+    ) : RegisterState() {
+        var currentContentIndex: Int by mutableStateOf(0)
+    }
+
+    data class Result(
+        val description: String? = ""
+    ): RegisterState()
+}
