d656949455cf312f6405bc1dc5d92d88374178f0
JinhoKang <jinho6187@gmail.com>
2022-07-16 23:09:48 +0900

    커스텀 font 추가

---
+++ app/src/main/java/com/ui/theme/EnglishTypography.kt
@@ -0,0 +1,63 @@
+package com.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object EnglishTypography {
+    val headline1: TextStyle = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp
+    )
+    val headline2 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp
+    )
+    val headline3 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    )
+    val headline4 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    )
+    val headline5 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    )
+    val subtitle1 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    )
+    val subtitle2 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    )
+    val subtitle3 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    )
+    val body1 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    val body2 = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+    val caption = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+}
--- app/src/main/java/com/ui/theme/Type.kt
+++ app/src/main/java/com/ui/theme/Type.kt
@@ -1,52 +1,37 @@
 package com.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.marryting.app.R
 
-val Typography = Typography(
-/*
-    h1 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Bold,
-        fontSize = 48.sp
+@OptIn(ExperimentalTextApi::class)
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratFont = GoogleFont(name = "Montserrat")
+
+@OptIn(ExperimentalTextApi::class)
+val MontserratFontFamily = FontFamily(
+    Font(googleFont = MontserratFont, fontProvider = provider),
+    Font(resId = R.font.montserrat_regular),
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.Light),
+    Font(resId = R.font.montserrat_light, weight = FontWeight.Light),
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.Medium),
+    Font(
+        resId = R.font.montserrat_medium,
+        weight = FontWeight.Medium
     ),
-    h2 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Bold,
-        fontSize = 34.sp
+    Font(googleFont = MontserratFont, fontProvider = provider, weight = FontWeight.SemiBold),
+    Font(
+        resId = R.font.montserrat_semibold,
+        weight = FontWeight.SemiBold
     ),
-    h3 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp
-    ),
-    h4 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
-    ),
-    subtitle1 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Medium,
-        fontSize = 16.sp
-    ),
-    subtitle2 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp
-    ),
-    body1 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    body2 = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    ),*/
 )
