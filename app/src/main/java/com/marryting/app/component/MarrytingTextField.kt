d7fb7c14b0120c27e5a7d695aa4ead4e47756e77
sooziini <soojinpar1026@gmail.com>
2022-07-22 15:25:56 +0900

    UserInfoScreen 이름, 직업 필드 작성 및 UserInfoItemState로 color 값 대응 설정

--- app/src/main/java/com/marryting/app/component/MarrytingTextField.kt
+++ app/src/main/java/com/marryting/app/component/MarrytingTextField.kt
@@ -23,29 +23,21 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.marryting.app.R
+import com.marryting.app.presentation.profile.UserInfoItemState
 import com.ui.theme.Color
 import com.ui.theme.DarkColor
 import com.ui.theme.KoreaTypography
 
 @Composable
-fun MarrytingTextField(modifier: Modifier = Modifier, value: String, onValueChanged: (String) -> Unit, onValueClear: () -> Unit, isActive: Boolean, isError: Boolean, label: String, placeholder: String) {
-    val currentColor = if (isActive) {
-        DarkColor.SubGreen
-    } else if (isError) {
-        DarkColor.ErrorRed
-    } else {
-        DarkColor.Grey400
-    }
-
+fun MarrytingTextField(modifier: Modifier = Modifier, value: String, onValueChanged: (String) -> Unit, onValueClear: () -> Unit, state: UserInfoItemState, label: String, placeholder: String) {
     Column {
         Box(
             modifier = modifier
                 .fillMaxWidth()
                 .then(
-                    if (isActive || isError) {
-                        Modifier.border(1.dp, currentColor, RoundedCornerShape(8.dp))
-                    } else {
-                        Modifier
+                    when (state) {
+                        UserInfoItemState.BASIC -> Modifier
+                        else -> Modifier.border(1.dp, state.color, RoundedCornerShape(8.dp))
                     }
                 )
                 .background(DarkColor.Grey700, RoundedCornerShape(8.dp))
@@ -55,7 +47,7 @@ fun MarrytingTextField(modifier: Modifier = Modifier, value: String, onValueChan
                 Text(
                     text = label,
                     style = KoreaTypography.caption,
-                    color = currentColor
+                    color = state.color
                 )
                 BasicTextField(
                     value = value,
@@ -80,23 +72,22 @@ fun MarrytingTextField(modifier: Modifier = Modifier, value: String, onValueChan
                 Box(
                     modifier = Modifier.align(Alignment.BottomEnd)
                 ) {
-                    if (isError) {
-                        ErrorIcon()
-                    } else {
-                        CloseButton(modifier = modifier) { onValueClear() }
+                    when (state) {
+                        UserInfoItemState.ERROR -> ErrorIcon()
+                        else -> CloseButton(modifier = modifier) { onValueClear() }
                     }
                 }
             }
         }
 
-        if (isError) {
+        if (state == UserInfoItemState.ERROR) {
             Box(
                 modifier = Modifier.padding(top = 4.dp, start = 20.dp, end = 20.dp)
             ) {
                 Text(
                     text = "error message",
                     style = KoreaTypography.caption,
-                    color = currentColor
+                    color = state.color
                 )
             }
         }
@@ -133,8 +124,7 @@ fun Prev() {
         value = text,
         onValueChanged = { if (text.length <= 5) text = it },
         onValueClear = { text = "" },
-        isActive = false,
-        isError = true,
+        state = UserInfoItemState.ERROR,
         label = "이름",
         placeholder = "이름을 입력해주세요"
     )
