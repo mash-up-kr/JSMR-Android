568f83fe70fc35252309e35662bf56f0e55e278e
sooziini <soojinpar1026@gmail.com>
2022-08-20 20:19:29 +0900

    PictureScreenItemType 파일 삭제

--- app/src/main/java/com/marryting/app/presentation/picture/PictureScreenItemType.kt
+++
@@ -1,8 +0,0 @@
-package com.marryting.app.presentation.picture
-
-import android.graphics.Bitmap
-
-interface PictureScreenItemType {
-    object AddPicture : PictureScreenItemType
-    data class ProfilePicture(val bitmap: Bitmap) : PictureScreenItemType
-}
